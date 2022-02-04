339fe47eb501af8e25b5847da70bf9239d474c3e
bmariem <mariem.borchani@gmail.com>
2022-02-04 06:13:05 +0100

    Display only 20 results of search

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,19 +1,20 @@
 import React from "react";
 
 const Search = ({ EmojiData, setEmojiList }) => {
+  const handleSearch = (event) => {
+    const newResults = EmojiData.filter((emoji) =>
+      emoji.keywords.includes(event.target.value)
+    );
+    setEmojiList(newResults.slice(0, 20));
+  };
+
   return (
     <header>
       <h1>😎 EmojiSearch 😎</h1>
       <input
         type="search"
         placeholder="What emoji are lookein for ?"
-        onChange={(event) => {
-          setEmojiList(
-            EmojiData.filter((emoji) => {
-              return emoji.keywords.includes(event.target.value);
-            })
-          );
-        }}
+        onChange={handleSearch}
       />
     </header>
   );
